b36c5ab8dfd1eff8edded3a4a5bf855ab959bfee
Arobs100 <a.robs100@gmail.com>
2022-08-07 13:27:48 +1200

    Convert html views into ejs templating engine files for dynamic content

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const path = require("path");
 
 const express = require("express");
-
-const adminData = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-
 const bodyParser = require("body-parser");
 
 const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,7 +18,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "" });
 });
 
 app.listen(3000);
